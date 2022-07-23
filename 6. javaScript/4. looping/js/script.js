013190d768857cca6a097c940e00b79bdaa4107f
kikiAgustin <kikiagustin62@gmail.com>
2022-07-23 07:54:55 +0600

    add

---
+++ 6. javaScript/4. looping/js/script.js
@@ -0,0 +1,11 @@
+const myArray = ["Kiki Agustin", "Mega Kusmayati", "David Abdul Aziz", "Isma"];
+
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
+
+console.log("batas array");
+
+for (const item of myArray) {
+    console.log(item);
+}
