74d56bfd3112f0a6df24dadcc21a9e8271768d4f
kikiAgustin <kikiagustin62@gmail.com>
2022-07-29 06:55:10 +0600

    tambah data

--- 5. calculator/assets/kalkulator.js
+++ 5. calculator/assets/kalkulator.js
@@ -1,1 +1,33 @@
-console.log("Selamat anda berhasil menggunakan javaScript pada website");
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    isWaitForSecondNumber: false
+};
+
+function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.isWaitForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+    calculator.displayNumber += digit;
+}
+
+const buttons = document.querySelectorAll('.button');
+
+for (const button of buttons) {
+    button.addEventListener('click', function (event) {
+
+        const target = event.target;
+
+        inputDigit(target.innerText);
+        updateDisplay();
+    });
+}
